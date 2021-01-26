18a8e9a0a24faf8998160d0b66a6c3da292c03ba
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-26 23:41:34 +0600

    Mathematical operations in JavaScript

---
+++ Module-1 Getting Started/mathematicalOperation.js
@@ -0,0 +1,22 @@
+var priceOne = 35;
+var priceTwo = 25;
+// var total = priceOne + priceTwo;
+// var total = priceOne - priceTwo;
+// var total = priceOne * priceTwo;
+// var total = priceOne / priceTwo;
+var total = priceOne % priceTwo;
+console.log(total);
+// priceTwo = priceTwo + 1;
+// priceTwo++;
+priceTwo--;
+console.log(priceTwo);
+
+var priceOne = 35;
+var name = "pen";
+var result = priceOne + name;
+console.log(result);
+
+var firstName = "Samiul";
+var secondName = "Sheikh";
+var result = firstName + ' ' + secondName;
+console.log(result);
