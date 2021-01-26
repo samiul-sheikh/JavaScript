09e6cc94c37e5cac1b314f5caf45009950a92108
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-26 23:56:15 +0600

    array add and remove element from the beginning and slice

---
+++ Module-2 JS Fundamental Concepts/addRemoveSlice.js
@@ -0,0 +1,16 @@
+var department = ['CSE', 'SE', 'CSSE', 'CIS', 'EEE', 'BBA', 'MBA'];
+// remove from first
+// department.shift();
+console.log(department);
+
+// add from first
+// department.unshift('BBA');
+// console.log(department);
+
+// start from index 2 to last
+// var dept = department.slice(2);
+// console.log(dept);
+
+// start from index 2 to 5
+var dept = department.slice(2, 5);
+console.log(dept);
