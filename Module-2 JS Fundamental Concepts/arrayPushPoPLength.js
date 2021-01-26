c2166c1385de769a4b64ce3e3b7b30ddf4d05bb4
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-26 23:54:53 +0600

    Array advanced, push, pop, array length

---
+++ Module-2 JS Fundamental Concepts/arrayPushPoPLength.js
@@ -0,0 +1,22 @@
+var friendsAge = [16,24,30,28];
+console.log(friendsAge);
+
+// for add last element in array
+friendsAge.push(22);
+friendsAge.push(12);
+console.log(friendsAge);
+
+// for define array lenght
+console.log(friendsAge.length);
+
+// for remove last element in array
+friendsAge.pop();
+console.log(friendsAge);
+
+// for remove first element in array
+friendsAge.shift();
+console.log(friendsAge);
+
+// for add first element in array
+friendsAge.unshift(5);
+console.log(friendsAge);
