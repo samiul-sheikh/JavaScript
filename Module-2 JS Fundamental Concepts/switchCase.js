d8b603b501ea71781f3f5fb7253d21fc6dda31e2
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-27 00:00:01 +0600

    Javascript switch case break and default

---
+++ Module-2 JS Fundamental Concepts/switchCase.js
@@ -0,0 +1,18 @@
+num = 20;
+switch(num){
+    case 1000:
+        console.log('I am 1000');
+        break;
+
+    case 100:
+        console.log('I am 100');
+        break;
+
+        case 50:
+        case 20:
+            console.log('I am 50 or 20');
+            break;
+
+        default:
+            console.log('I dont know the value!');
+}
