d86eb1eb697b060e58c979e81d11e67aa49da93d
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-26 23:35:48 +0600

    explore string case change index split

---
+++ Module-1 Getting Started/stringCaseChange.js
@@ -0,0 +1,5 @@
+var promise = "I promise i will work HARD to become a programmer";
+// console.log(promise.toLowerCase());
+// console.log(promise.toUpperCase());
+// console.log(promise.indexOf('will'));
+console.log(promise.split('work'));
