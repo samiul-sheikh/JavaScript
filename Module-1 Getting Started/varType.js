e4c3a7c67575ce198f09b16a0485641c3b2607da
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-26 23:30:45 +0600

    Variable type, Numeric, String, Boolean

---
+++ Module-1 Getting Started/varType.js
@@ -0,0 +1,11 @@
+var seenAfter = 21;
+console.log(typeof seenAfter);
+
+var name = "Samiul Sheikh";
+console.log(typeof name);
+
+var isHot = true;
+console.log(isHot);
+
+var isRich = false;
+console.log(typeof isRich);
