c0293f4a4de0072ac710d3266de3f6b11557a32e
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-26 23:49:03 +0600

    Multiple conditions, fulfill both conditions, else if

---
+++ Module-1 Getting Started/conditional.js
@@ -0,0 +1,12 @@
+var gettingJob = true;
+var savingAmount = 200000;
+
+if(gettingJob == true && savingAmount > 500000 ){
+    console.log("Lets see girl for bride");
+}
+else if(gettingJob == true){
+    console.log("wait a few days");
+}
+else{
+    console.log("nothing to do");
+}
