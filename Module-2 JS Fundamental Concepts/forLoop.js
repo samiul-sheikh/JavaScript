d0902f173ca5c469fa2c3924bfa3bc52c202e5e9
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-26 23:58:44 +0600

    For loop, run a loop for each element of an array

---
+++ Module-2 JS Fundamental Concepts/forLoop.js
@@ -0,0 +1,17 @@
+// for (var i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+var num = [10, 20, 30, 40, 50, 60, 70];
+for(i = 0; i < num.length; i++){
+    var element = num[i];
+    console.log(element);
+}
+
+
+
+var friends = ['nayeem', 'nadim', 'ekra', 'urmi'];
+for (i = 0; i < friends.length; i++){
+    var name = friends[i];
+    console.log(name);
+}
