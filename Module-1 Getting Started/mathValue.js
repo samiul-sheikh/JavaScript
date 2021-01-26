9bea820b91952e457945ab6dc0ba740badb981ff
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-26 23:43:48 +0600

    Math absolute round floor ceil random

---
+++ Module-1 Getting Started/mathValue.js
@@ -0,0 +1,21 @@
+var number = -5;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
+
+
+
+var number = 50.95555;
+var result = Math.round(number);
+// For increase
+// var result = Math.ceil(number);
+
+// for decrese
+// var result = Math.floor(number);
+
+console.log(result);
+
+
+// for random
+var number = Math.random() * 100;
+var result = Math.round(number);
+console.log(result);
