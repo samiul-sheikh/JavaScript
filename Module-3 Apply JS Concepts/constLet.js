43228b90d130bcb64f5c2c8e5096dc4d6cd57391
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-28 22:46:07 +0600

    variable let and const and how to use them

---
+++ Module-3 Apply JS Concepts/constLet.js
@@ -0,0 +1,19 @@
+// var name = 'Samiul Sheikh';
+// // console.log(name.length);
+
+
+var name = 'Samiul Sheikh';
+
+if(name.length > 4){
+    name = 'Sami';
+}
+console.log(name);
+
+// if need to fix use constant
+const country = "Bangladesh";
+console.log(country);
+
+// if need to change after use let
+let age = 25;
+age = 27;
+console.log(age);
