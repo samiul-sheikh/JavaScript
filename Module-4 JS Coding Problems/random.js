4f073a6527616eeedbb8779a3f2b0990157872cc
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-01-31 23:25:41 +0600

    Random number, random number between

---
+++ Module-4 JS Coding Problems/random.js
@@ -0,0 +1,20 @@
+var num = 2.4047;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+var randomNum = Math.random() * 10;
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(randomNum);
+
+var randomNum = Math.random() * 10;
+var output = Math.round(randomNum);
+console.log(output);
+
+// using loop print all values
+for (var i = 0; i < 10; i++) {
+    var randomNum = Math.random() * 6;
+    var output = Math.round(randomNum);
+    console.log(output);
+}
