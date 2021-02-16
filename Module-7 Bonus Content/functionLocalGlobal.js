c143bbf56cb8e2e78675c68b0758eef721d8c494
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-16 23:17:39 +0600

    Global vs local variable IIFE function expression vs declaration

---
+++ Module-7 Bonus Content/functionLocalGlobal.js
@@ -0,0 +1,25 @@
+function addTodo(task){
+    // local variable
+    const newTaskElement = document.createElement('li');
+}
+
+// function expression
+// var myTodo(task){
+
+// }
+
+// global variable
+var name = 'Samiul';
+
+function addUser(){
+    var userName = 'sany';
+    console.log(name);
+}
+addUser();
+// console.log(userName);
+
+// immediate invoking function expression(IIFE) - special function 
+// (function(){
+//     var userName = 'sagor';
+//     console.log(userName);
+// })();
